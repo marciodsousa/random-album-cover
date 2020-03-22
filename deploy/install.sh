effa3cddff98053790cfbf4717bd7d8d3a2e772e
Márcio de Sousa <marciodesousa16@gmail.com>
2020-03-22 23:06:18 +0100

    changed filename

--- deploy/install.sh
+++ deploy/install.sh
@@ -2,9 +2,9 @@
 set -x # Show the output of the following commands (useful for debugging)
     
 # Import the SSH deployment key
-rm do_deploy_key.enc # Don't need it anymore
-chmod 600 do_deploy_key
-mv do_deploy_key ~/.ssh/id_rsa
+rm travis_ssh_key.enc # Don't need it anymore
+chmod 600 travis_ssh_key
+mv travis_ssh_key ~/.ssh/id_rsa
 
 npm install -g yarn
 
